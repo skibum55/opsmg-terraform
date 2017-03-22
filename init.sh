77653c5ff2a851ded9ba0430aa32ca7d514925c5
Sean Keery <seankeery@Seans-Mac-2.local>
2017-03-22 12:09:34 -0600

    ignore .terraform directory

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # https://github.com/hashicorp/terraform/issues/3692
-terraform init --backend=true \
-    --backend-config="region=${TF_VAR_aws_region}" \
+terraform init --backend-config="region=${TF_VAR_aws_region}" \
     --backend-config="bucket=${TF_VAR_tf_s3_bucket}" \
     --backend-config="key=${TF_VAR_environment}/${TF_VAR_master_state_file}" \
     --backend-config="access_key=${TF_VAR_aws_access_key}" \
     --backend-config="secret_key=${TF_VAR_aws_secret_key}" \
-    --backend-config="encrypt=1"
+    --backend-config="encrypt=true"
     #--backend-config=s3backend.config
     # --backend=s3
